67cfcb3631f73e58f5338f23c397bb62b61ca04f
dongphil.yoo <dongphil.yoo@US-NYC-C02Z80Q9LVDL.globalservs.com>
2022-03-03 10:51:23 -0500

    updated fetch api

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton'
 
 const Products = () => {
   const [data, setData] = useState([])
@@ -10,30 +11,126 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true)
-      const response = await fetch(
-        'https://asos2.p.rapidapi.com/products/v3/detail?id=9851612&lang=en-US&store=US&sizeSchema=US&currency=USD',
-        {
-          method: 'GET',
-          headers: {
-            'x-rapidapi-host': 'asos2.p.rapidapi.com',
-            'x-rapidapi-key':
-              '49f0efef13mshea42d28781e5530p1b4408jsne991abab5818',
-          },
-        }
-      )
-      // .then((response) => {
-      //   console.log(response.properties)
-      // })
-      // .catch((err) => {
-      //   console.error(err)
-      // })
-      const data = await response.json()
-      console.log(data)
+
+      const response = await fetch('https://fakestoreapi.com/products')
+
+      if (componentMounted) {
+        setData(await response.clone().json())
+        setFilter(await response.json())
+        setLoading(false)
+        console.log(data)
+      }
+
+      return () => {
+        componentMounted = false
+      }
     }
     getProducts()
   }, [])
 
-  return <div></div>
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    )
+  }
+
+  const filterProduct = (category) => {
+    const updatedList = data.filter((p) => p.category === category)
+    setFilter(updatedList)
+  }
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button
+            className="btn btn-outline-dark"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark ms-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark ms-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark ms-2"
+            onClick={() => filterProduct('jewelery')}
+          >
+            Jewelery
+          </button>
+          <button
+            className="btn btn-outline-dark ms-2"
+            onClick={() => filterProduct('electronics')}
+          >
+            Electronic
+          </button>
+        </div>
+        {filter.map((product, idx) => (
+          <>
+            <div className="col-md-3 mb-4" key={idx}>
+              <div className="card h-100 text-center p-4" key={product.id}>
+                <img
+                  src={product.image}
+                  className="card-img-top"
+                  alt={product.title}
+                  height="250px"
+                />
+                <div className="card-body">
+                  <h5 className="card-title mb-0">
+                    {product.title.substring(0, 24)}...
+                  </h5>
+                  <p className="card-text lead fw-bold">
+                    {'$' + product.price}
+                  </p>
+                  <a href="#" className="btn btn-outline-dark">
+                    Buy Now
+                  </a>
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Products
